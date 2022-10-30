6240367c8553f2fcd68a3b4d1ff5f272af7b4f94
avstreamdevpro <avstreamdevpro@hotmail.com>
2022-10-31 00:59:37 +0800

    connected view model and View

--- app/src/main/java/com/huntcontrol/mvvmsample/MainActivity.kt
+++ app/src/main/java/com/huntcontrol/mvvmsample/MainActivity.kt
@@ -2,10 +2,56 @@ package com.huntcontrol.mvvmsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var mainRecycler: RecyclerView
+    private lateinit var btnSubmit: Button
+    private lateinit var edtTitle: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSubmit = findViewById(R.id.btnSubmit)
+        edtTitle = findViewById(R.id.edtTitle)
+        mainRecycler = findViewById(R.id.mainRecycler)
+
+        viewModel = MainViewModel()
+        btnSubmit.setOnClickListener {
+            addData()
+        }
+        initialiseAdapter()
+    }
+
+
+    private fun initialiseAdapter() {
+        // init custom layout manager or etc
+        observeData()
+    }
+
+    private fun observeData() {
+        viewModel.fst.observe(this, Observer {
+            mainRecycler.adapter = NoteRecyclerAdapter(viewModel, it, this)
+        })
+    }
+
+    private fun addData() {
+        val title = edtTitle.text.toString()
+        if (title.isNullOrEmpty()) {
+            Toast.makeText(this, "Please enter title!", Toast.LENGTH_LONG).show()
+        } else {
+            val blog = Blog(title)
+            viewModel.add(blog)
+            edtTitle.text.clear()
+            mainRecycler.adapter?.notifyDataSetChanged()
+        }
     }
 }
